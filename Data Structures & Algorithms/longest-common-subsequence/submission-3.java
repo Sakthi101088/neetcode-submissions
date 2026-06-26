952678309c20b0bbe056a7ca4bf3166e42a7839b
Sakthi101088 <36725966+Sakthi101088@users.noreply.github.com>
2026-06-26 14:09:13 +0530

    Add: longest-common-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-3.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+
+        char[] tex1 = text1.toCharArray();
+        char[] tex2 = text2.toCharArray();
+        
+        if(tex2.length <tex1.length)
+            return longest(tex1,tex2);
+        else
+            return longest(tex2,tex1);
+        
+    }
+    public int longest(char[] tex1, char[] tex2){
+        int count = 0;
+        int index = 0;
+        for(int j =0;j<tex2.length;j++){
+            for(int i =index;i<tex1.length;i++){
+                if(tex1[i] == tex2[j]){
+                    count++;
+                    index = i+1;
+                    break;
+                }
+            }
+            if(count==tex2.length)
+                break;
+        }
+        return count;
+    }
+}
